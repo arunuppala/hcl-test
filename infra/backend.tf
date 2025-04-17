23607995c732b458dea044e33c85a406f0a45f58
arunuppala <arunuppala09@gmail.com>
2025-04-17 23:59:29 +0530

    region changed

--- infra/backend.tf
+++ infra/backend.tf
@@ -32,7 +32,7 @@ terraform {
   backend "s3" {
     bucket       = "terraform-state-bucket"
     key          = "terraform.tfstate"
-    region       = "ap-south-1"
+    region       = "us-east-1"
     encrypt      = true
     use_lockfile = true
   }
