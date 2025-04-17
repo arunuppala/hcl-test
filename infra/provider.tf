88c20a0cfe0d2dce8cdf98a97f8d8abaa595a2ee
arunuppala <arunuppala09@gmail.com>
2025-04-18 02:10:58 +0530

    test

--- infra/provider.tf
+++ infra/provider.tf
@@ -1,14 +1,14 @@
 terraform {
+  required_version = ">= 1.3"
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "5.94.1"
+      version = "~> 5.94"  # Use ~> for safe upgrades within major versions
     }
   }
 }
 
 provider "aws" {
-  region = "us-east-1"
-  # access_key = "<<access_key>>"
-  # secret_key = "<<secret_key>>"
+  region = var.region
 }
