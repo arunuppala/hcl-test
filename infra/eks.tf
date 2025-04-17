de377b516790415349c0c11d80ed25b6ed64c827
arunuppala <arunuppala09@gmail.com>
2025-04-18 02:16:08 +0530

    test

--- infra/eks.tf
+++ infra/eks.tf
@@ -43,11 +43,6 @@ module "eks" {
     }
   }
 
-  cluster_encryption_config = [{
-    provider_key_arn = "ac01234b-00d9-40f6-ac95-e42345f78b00"
-    resources        = ["secrets"]
-  }]
-
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets
 
