9d5af6b5ab2fe33c94fa207364a0fbf679a208ec
arunuppala <arunuppala09@gmail.com>
2025-04-18 00:21:37 +0530

    testing

--- infra/backend.tf
+++ infra/backend.tf
@@ -1,32 +1,32 @@
-resource "aws_s3_bucket" "state" {
-  bucket        = "terraform-state-bucket"
-  force_destroy = true
+# resource "aws_s3_bucket" "state" {
+#   bucket        = "terraform-state-bucket"
+#   force_destroy = true
 
-  tags = {
-    Name = "My bucket"
-  }
+#   tags = {
+#     Name = "My bucket"
+#   }
 
-  lifecycle {
-    prevent_destroy = true
-  }
-}
+#   lifecycle {
+#     prevent_destroy = true
+#   }
+# }
 
-resource "aws_s3_bucket_versioning" "name" {
-  bucket = aws_s3_bucket.state.id
-  versioning_configuration {
-    status = "Enabled"
-  }
-}
+# resource "aws_s3_bucket_versioning" "name" {
+#   bucket = aws_s3_bucket.state.id
+#   versioning_configuration {
+#     status = "Enabled"
+#   }
+# }
 
-resource "aws_s3_bucket_server_side_encryption_configuration" "tf_state" {
-  bucket = aws_s3_bucket.state.id
+# resource "aws_s3_bucket_server_side_encryption_configuration" "tf_state" {
+#   bucket = aws_s3_bucket.state.id
 
-  rule {
-    apply_server_side_encryption_by_default {
-      sse_algorithm = "AES256"
-    }
-  }
-}
+#   rule {
+#     apply_server_side_encryption_by_default {
+#       sse_algorithm = "AES256"
+#     }
+#   }
+# }
 
 terraform {
   backend "s3" {
