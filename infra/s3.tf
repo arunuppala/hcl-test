3600a594aceb97c550034ce94e3ef21e29c133e2
arunuppala <arunuppala09@gmail.com>
2025-04-18 09:34:34 +0530

    s3 commented

--- infra/s3.tf
+++ infra/s3.tf
@@ -1,40 +1,40 @@
-resource "aws_s3_bucket" "state" {
-  bucket        = "terraform-hcl-test-state"
-  force_destroy = true
+# resource "aws_s3_bucket" "state" {
+#   bucket        = "terraform-hcl-test-state"
+#   force_destroy = true
 
-  tags = {
-    Name        = "Terraform State Bucket"
-    Environment = var.env
-  }
+#   tags = {
+#     Name        = "Terraform State Bucket"
+#     Environment = var.env
+#   }
 
-  lifecycle {
-    prevent_destroy = true
-  }
-}
+#   lifecycle {
+#     prevent_destroy = true
+#   }
+# }
 
-resource "aws_s3_bucket_versioning" "state_versioning" {
-  bucket = aws_s3_bucket.state.id
+# resource "aws_s3_bucket_versioning" "state_versioning" {
+#   bucket = aws_s3_bucket.state.id
 
-  versioning_configuration {
-    status = "Enabled"
-  }
-}
+#   versioning_configuration {
+#     status = "Enabled"
+#   }
+# }
 
-resource "aws_s3_bucket_server_side_encryption_configuration" "state_encryption" {
-  bucket = aws_s3_bucket.state.id
+# resource "aws_s3_bucket_server_side_encryption_configuration" "state_encryption" {
+#   bucket = aws_s3_bucket.state.id
 
-  rule {
-    apply_server_side_encryption_by_default {
-      sse_algorithm = "AES256"
-    }
-  }
-}
+#   rule {
+#     apply_server_side_encryption_by_default {
+#       sse_algorithm = "AES256"
+#     }
+#   }
+# }
 
-resource "aws_s3_bucket_public_access_block" "state_block_public" {
-  bucket = aws_s3_bucket.state.id
+# resource "aws_s3_bucket_public_access_block" "state_block_public" {
+#   bucket = aws_s3_bucket.state.id
 
-  block_public_acls       = true
-  block_public_policy     = true
-  ignore_public_acls      = true
-  restrict_public_buckets = true
-}
+#   block_public_acls       = true
+#   block_public_policy     = true
+#   ignore_public_acls      = true
+#   restrict_public_buckets = true
+# }
