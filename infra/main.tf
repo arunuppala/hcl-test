86c64e9f06b1f233dcd12d0acc4b472dab6c027d
arunuppala <arunuppala09@gmail.com>
2025-04-18 09:39:47 +0530

    push

--- infra/main.tf
+++ infra/main.tf
@@ -22,7 +22,7 @@ module "ecr" {
 
   repository_name = "hcl-test"
 
-  repository_read_write_access_arns = ["arn:aws:iam::012345678901:role/terraform"]
+  # repository_read_write_access_arns = ["arn:aws:iam::012345678901:role/terraform"]
   repository_lifecycle_policy = jsonencode({
     rules = [
       {
